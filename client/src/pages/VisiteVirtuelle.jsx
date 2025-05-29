3ca93a33bda3345e84ca999f31aea457305b0bd2
KAMAL TAGHLAOUI <102923258+Kamal-OS@users.noreply.github.com>
2025-05-29 04:54:22 +0100

    feat(client): add 'visite virtuelle' using iframe from Lapentor

---
+++ client/src/pages/VisiteVirtuelle.jsx
@@ -0,0 +1,20 @@
+function VisiteVirtuelle() {
+  return (
+    <>
+      <iframe
+        src="https://app.lapentor.com/sphere/ista-med-el-fassi-errachidia"
+        frameborder="0"
+        width="100%"
+        height="800px"
+        scrolling="no"
+        allow="vr,gyroscope,accelerometer"
+        allowfullscreen="true"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        oallowfullscreen="true"
+        msallowfullscreen="true"></iframe>
+    </>
+  );
+}
+
+export default VisiteVirtuelle;
